280269105c1cc1a7713d9365c27eb51a1a6c379d
wnjoon <wnjoon@gmail.com>
2023-07-07 18:15:04 +0900

    update

--- tessera/run.sh
+++ tessera/run.sh
@@ -6,21 +6,25 @@ echo "==== Generate key pair ===="
 
 if [ ! -d keys/key${ID} ]; then
       mkdir -p keys/key${ID}
+      tessera -keygen -filename ${PWD}/keys/key${ID}/key
 fi
 
-tessera -keygen -filename ${PWD}/keys/key${ID}/key
+
 
 echo "\n==== Generate tessera config file ===="
 
-if [ ! -d tessera${ID} ]; then
-      mkdir -p tessera${ID}
+if [ -d tessera${ID} ]; then
+      rm -Rf tessera${ID}
 fi
 
+mkdir -p tessera${ID}
 cp config_template.json config.json
 
 sed -i "s|_id_|${ID}|g" "config.json"
 
 mv config.json tessera${ID}
+
+
 
 echo "\n==== Run tessera node ===="
 
