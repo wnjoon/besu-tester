262681fd0f1de055c4a12b3f032e0b474891599e
wnjoon <wnjoon@gmail.com>
2023-08-16 17:05:00 +0900

    remove caliper

--- caliper/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npx caliper launch manager \
-    --caliper-bind-sut besu:1.4 \
-    --caliper-workspace . \
-    --caliper-benchconfig benchmarks/scenario/simple/config.yaml \
-    --caliper-networkconfig networks/besu/qbft.json
